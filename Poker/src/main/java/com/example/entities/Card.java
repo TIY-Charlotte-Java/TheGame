34c36029994f42a3f9fb6af9c803652cc9f6ee76
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-18 10:29:34 -0400

    Pushing to tiy

--- Poker/src/main/java/com/example/entities/Card.java
+++ Poker/src/main/java/com/example/entities/Card.java
@@ -1,9 +1,48 @@
 package com.example.entities;
 
+import java.util.ArrayList;
+
 /**
  * Created by Ultramar on 5/18/16.
  */
 public class Card {
 
-    public enum Suites
+    String rank;
+    Suites suite;
+
+
+    public enum Suites {
+        HEARTS,
+        CLUBS,
+        DIMONDS,
+        SPADES
+
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public Suites getSuite() {
+        return suite;
+    }
+
+    public void setSuite(Suites suite) {
+        this.suite = suite;
+    }
+
+    public Card(String rank, Suites suite) {
+        this.rank = rank;
+        this.suite = suite;
+    }
+
+    public static ArrayList<Card> buildDeck() {
+        ArrayList<Card> Deck = new ArrayList<>();
+
+
+    }
 }
