0ea16fe38742b1e24c4d266869bae3d6ba5f7ea2
Ben Sterrett <raynjamin@gmail.com>
2016-05-19 16:21:22 -0400

    Adding a bunch of CSS things.

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,97 @@
-var stompClient = null;
+$(document).ready(function () {
+    /* global Deck */
+    console.log("things");
+    var prefix = Deck.prefix
 
-function setConnected(connected) {
-    console.log("Connected.");
-}
+    var transform = prefix('transform')
 
-function connect() {
-    var socket = new SockJS('/cards');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function(frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/card-data/cards', function(greeting){
-            showGreeting(JSON.parse(greeting.body).content);
-        });
-    });
-}
+    var translate = Deck.translate
 
-function disconnect() {
-    if (stompClient != null) {
-        stompClient.disconnect();
+    var $container = document.getElementById('container')
+    var $topbar = document.getElementById('topbar')
+
+    $shuffle.textContent = 'Shuffle'
+    $sort.textContent = 'Sort'
+    $bysuit.textContent = 'By suit'
+    $fan.textContent = 'Fan'
+    $poker.textContent = 'Poker'
+    $flip.textContent = 'Flip'
+
+    $topbar.appendChild($flip)
+    $topbar.appendChild($shuffle)
+    $topbar.appendChild($bysuit)
+    $topbar.appendChild($fan)
+    $topbar.appendChild($poker)
+    $topbar.appendChild($sort)
+
+    var deck = Deck()
+
+
+    $shuffle.addEventListener('click', function () {
+      deck.shuffle()
+      deck.shuffle()
+    })
+    $sort.addEventListener('click', function () {
+      deck.sort()
+    })
+    $bysuit.addEventListener('click', function () {
+      deck.sort(true) // sort reversed
+      deck.bysuit()
+    })
+    $fan.addEventListener('click', function () {
+      deck.fan()
+    })
+    $flip.addEventListener('click', function () {
+      deck.flip()
+    })
+    $poker.addEventListener('click', function () {
+      deck.queue(function (next) {
+        deck.cards.forEach(function (card, i) {
+          setTimeout(function () {
+            card.setSide('back')
+          }, i * 7.5)
+        })
+        next()
+      })
+      deck.shuffle()
+      deck.shuffle()
+      deck.poker()
+    })
+
+    deck.mount($container)
+
+    deck.intro()
+    deck.sort()
+
+    function printMessage (text) {
+      var animationFrames = Deck.animationFrames
+      var ease = Deck.ease
+      var $message = document.createElement('p')
+      $message.classList.add('message')
+      $message.textContent = text
+
+      document.body.appendChild($message)
+
+      $message.style[transform] = translate(window.innerWidth + 'px', 0)
+
+      var diffX = window.innerWidth
+
+      animationFrames(1000, 700)
+        .progress(function (t) {
+          t = ease.cubicInOut(t)
+          $message.style[transform] = translate((diffX - diffX * t) + 'px', 0)
+        })
+
+      animationFrames(6000, 700)
+        .start(function () {
+          diffX = window.innerWidth
+        })
+        .progress(function (t) {
+          t = ease.cubicInOut(t)
+          $message.style[transform] = translate((-diffX * t) + 'px', 0)
+        })
+        .end(function () {
+          document.body.removeChild($message)
+        })
     }
-    setConnected(false);
-    console.log("Disconnected");
-}
-
-function sendName() {
-    stompClient.send("/app/cards");
-}
-
-function showGreeting(message) {
-    console.log(message);
-}
+});
